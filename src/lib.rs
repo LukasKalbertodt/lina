96449d58d4f1f545957fb4cd0e04a8c6ed3e4360
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-09-10 10:33:14 +0200

    Reexport important types and functions from `vec`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
 
 pub mod vec;
 pub mod num;
+
+pub use self::{
+    vec::{
+        Point, Point2, Point2f, Point3, Point3f,
+        Vector, Vec2, Vec2f, Vec3, Vec3f, Vec4, Vec4f,
+        point2, point3, vec2, vec3, vec4,
+    },
+};
