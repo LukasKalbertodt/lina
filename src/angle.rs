8f27d3b03f27d0b83de6bf7f79854883bf6e4f07
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-09-11 12:32:49 +0200

    Add `Angle` trait with lots of angle related functions

--- src/angle.rs
+++ src/angle.rs
@@ -1,21 +1,80 @@
+use std::fmt::Debug;
+use num_traits::Float as _;
+
 use crate::Float;
 
 
-/// An angle in radians. A full rotation is 2π radians.
-pub struct Radians<T: Float>(pub T);
+/// Values representing an angle. Implemented by [`Radians`] and [`Degrees`].
+pub trait Angle: From<Radians<Self::Unitless>> + From<Degrees<Self::Unitless>> + Copy + Debug {
+    type Unitless: Float;
 
-/// An angle in degree. A full rotation is 360°.
-pub struct Degrees<T: Float>(pub T);
+    /// A full rotation: 2π radians or 360°.
+    fn full_turn() -> Self;
 
-impl<T: Float> Radians<T> {
-    pub fn to_degrees(self) -> Degrees<T> {
-        Degrees(self.0.to_degrees())
+    fn to_radians(self) -> Radians<Self::Unitless>;
+    fn to_degrees(self) -> Degrees<Self::Unitless>;
+    fn unitless(self) -> Self::Unitless;
+
+    fn sin(self) -> Self::Unitless {
+        self.to_radians().unitless().sin()
+    }
+    fn cos(self) -> Self::Unitless {
+        self.to_radians().unitless().cos()
+    }
+    fn tan(self) -> Self::Unitless {
+        self.to_radians().unitless().tan()
+    }
+
+    fn asin(v: Self::Unitless) -> Self {
+        Radians(v.asin()).into()
+    }
+    fn acos(v: Self::Unitless) -> Self {
+        Radians(v.acos()).into()
+    }
+    fn atan(v: Self::Unitless) -> Self {
+        Radians(v.atan()).into()
     }
 }
 
-impl<T: Float> Degrees<T> {
-    pub fn to_radians(self) -> Radians<T> {
+/// An angle in radians. A full rotation is 2π radians.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Radians<T: Float>(pub T);
+
+/// An angle in degree. A full rotation is 360°.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Degrees<T: Float>(pub T);
+
+impl<T: Float> Angle for Radians<T> {
+    type Unitless = T;
+
+    fn full_turn() -> Self {
+        Self(T::PI() + T::PI())
+    }
+    fn to_radians(self) -> Radians<Self::Unitless> {
+        self
+    }
+    fn to_degrees(self) -> Degrees<T> {
+        Degrees(self.0.to_degrees())
+    }
+    fn unitless(self) -> Self::Unitless {
+        self.0
+    }
+}
+
+impl<T: Float> Angle for Degrees<T> {
+    type Unitless = T;
+
+    fn full_turn() -> Self {
+        Self(num_traits::cast(360.0).unwrap())
+    }
+    fn to_radians(self) -> Radians<T> {
         Radians(self.0.to_radians())
+    }
+    fn to_degrees(self) -> Degrees<T> {
+        self
+    }
+    fn unitless(self) -> Self::Unitless {
+        self.0
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ mod util;
 pub mod named_scalar;
 
 pub use self::{
-    angle::{Degrees, Radians},
+    angle::{Angle, Degrees, Radians},
     vec::{
         point::{Point, Point2, Point2f, Point3, Point3f, point2, point3},
         vector::{Vector, Vec2, Vec2f, Vec3, Vec3f, Vec4, Vec4f, vec2, vec3, vec4},
